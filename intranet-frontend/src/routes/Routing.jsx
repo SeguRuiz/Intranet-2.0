ca19ed1d7a0a771845dac7cea830063f6dbae27b
W-Alexander <alexanderaguilar726@gmail.com>
2024-10-01 08:46:41 -0600

    arreglo de rutas

--- intranet-frontend/src/routes/Routing.jsx
+++ intranet-frontend/src/routes/Routing.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Loginpage } from "../pages/Loginpage";
-import Home_student_page from "../pages/Home_student_page";
+import Home_student_page from "../pages/cursos";
 import Admin from "../pages/Admin";
+import Home from "../pages/home";
 
 export const Routing = () => {
   return (
@@ -9,7 +10,8 @@ export const Routing = () => {
       <Router>
         <Routes>
           <Route path="/" element={<Loginpage />} />
-          <Route path="/home" element={<Home_student_page />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cursos" element={<Home_student_page />} />
           <Route path="/admin" element={<Admin />} />
         </Routes>
       </Router>
