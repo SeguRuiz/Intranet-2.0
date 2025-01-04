e421b00ed9294432fa550f9eff07f5723e4a48e4
SeguRuiz <ruizsegura96199@gmail.com>
2025-01-04 10:59:38 -0600

    La pantalla de carga se volvio un componente aparte

--- intranet-frontend/src/pages/info_cursos/Info_cursos.jsx
+++ intranet-frontend/src/pages/info_cursos/Info_cursos.jsx
@@ -1,7 +1,7 @@
 import "./Info_cursos.css";
 import { Sidemenu } from "../../components/SideMenu/Sidemenu";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { setContenidos } from "../../redux/CursosContenidosSlice";
 import { set_archivo_mostrandose } from "../../redux/CursosContenidosSlice";
 import { useEffect } from "react";
@@ -12,15 +12,15 @@ import Go_to_admin from "../../components/admin_actions_cursos/go_to_admin";
 import { useFetch } from "../../services/llamados";
 import { getCookie } from "../../utils/Cookies";
 import { useState } from "react";
-import { Button, LinearProgress } from "@mui/material";
-import NOT_FOUND_SVG from "../../assets/Empty/404.svg";
+import { LinearProgress } from "@mui/material";
+import Info_cursos_404 from "./Info_cursos_404";
 
 const Info_cursos = () => {
   const { userInSession } = useSelector((x) => x.Auth);
   const [idValido, setIdValido] = useState(false);
   const [notFound, setNotFound] = useState(false);
   const accion = useDispatch();
-  const navigate = useNavigate();
+
   const { id_curso } = useParams();
   const token = getCookie("token");
   const { fetch_the_data, fetching } = useFetch();
@@ -70,41 +70,7 @@ const Info_cursos = () => {
             </div>
           </div>
         ) : (
-          notFound &&
-          !fetching && (
-            <>
-              <div className="not-found-cursos-container">
-                <div className="not-found-message-cont">
-                  <h1>404!</h1>
-                  <p>
-                    El curso ya no esta disponible o solo andabas de curioso...{" "}
-                  </p>
-                  <Button
-                    variant="outlined"
-                    onClick={() => {
-                      navigate("/cursos");
-                    }}
-                    sx={{
-                      alignSelf: "flex-start",
-                      marginTop: 3,
-                      borderColor: "var(--PrymaryContainer-color)",
-                      color: "var(--PrymaryContainer-color)",
-                    }}
-                  >
-                    Volver al inicio
-                  </Button>
-                </div>
-
-                <div className="not-found-img-container">
-                  <img
-                    src={NOT_FOUND_SVG}
-                    alt="error 404.svg"
-                    className="not-found-img"
-                  />
-                </div>
-              </div>
-            </>
-          )
+          notFound && !fetching && <Info_cursos_404 />
         )}
       </div>
     </>
